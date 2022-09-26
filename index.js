3eb89c8d98baa55300f6014ab498e5d33fee19ab
Benjamin Zalasky <benjamin.zalasky@lookout.com>
2022-09-26 13:09:31 -0500

    Update code transformer for Jest 28+

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  process: (content) => "module.exports = " + JSON.stringify(content),
+  // process() and processAsync() methods of a custom transformer module cannot
+  // return a string anymore. They must always return an object.
+  //
+  // https://jestjs.io/docs/28.x/upgrading-to-jest28#transformer
+  process: (content) => ({
+    code: "module.exports = " + JSON.stringify(content),
+  }),
 };
